b9139efc85a68b5e63615b16a2436bf502c77332
DnoYT <56258350+DnoYT@users.noreply.github.com>
2020-12-21 00:07:26 +0800

    main.cpp
    添加代码

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	printf("hello world");
+	printf("hello world\n");
+	printf("hello world2");
 	return 0;
 }
